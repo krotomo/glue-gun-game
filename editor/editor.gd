c3a5cb93d29819d5cdb3f0ea0732ee215dc8b8ef
Ken Otomo <ken.otomo@stonybrook.edu>
2024-12-11 22:05:04 -0500

    adding buttons

--- editor/editor.gd
+++ editor/editor.gd
@@ -4,7 +4,9 @@ extends VSplitContainer
 @onready var add_layer_button = $HSplitContainer/LayerPanel/LayerPanelButtons/AddLayerButton
 @onready var remove_layer_button = $HSplitContainer/LayerPanel/LayerPanelButtons/RemoveLayerButton
 @onready var layer_list = $HSplitContainer/LayerPanel/LayerList
-
+var layer_id = 0
+var layer_button_group = ButtonGroup.new()
+var selected_layer_id = -1
 
 func _ready():
 	add_layer_button.pressed.connect(add_layer)
@@ -12,8 +14,19 @@ func _ready():
 
 
 func add_layer():
-	layer_list.add_item("Layer " + str(layer_list.get_item_count() + 1))
+	var id = layer_id
+	layer_id += 1
+
+	var button = Button.new()
+	button.toggle_mode = true
+	button.button_group = layer_button_group
+	button.text = "Layer " + str(id)
+	layer_list.add_child(button)
 
 
 func remove_layer():
-	layer_list.remove_item(layer_list.get_selected_id())
+	for child in layer_list.get_children():
+		if child is Button:
+			if child.button_pressed:
+				child.queue_free()
+				break
