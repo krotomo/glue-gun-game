78d17fab99306293439589585def9a27d6338278
Ken Otomo <ken.otomo@stonybrook.edu>
2024-12-21 20:10:54 -0500

    buttons wow

--- editor/editor.gd
+++ editor/editor.gd
@@ -1,7 +1,12 @@
 extends VSplitContainer
 
+const TileButton = preload("res://editor/tile_button.gd")
+
 @export var tile_set: TileSet
-@onready var tile_container = $HSplitContainer/VBoxContainer/TileContainer
+@export var outline_texture: Texture2D
+@onready var tile_container = $HSplitContainer/VBoxContainer/ScrollContainer/MarginContainer/TileContainer
+@onready var subviewport_container = $HSplitContainer/SubViewportContainer
+var button_group = ButtonGroup.new()
 
 
 func _ready():
@@ -10,13 +15,35 @@ func _ready():
 		var source : TileSetAtlasSource = tile_set.get_source(source_id)
 		var texture = source.texture
 		var dimensions = source.get_atlas_grid_size()
-		print(dimensions)
 		for row in range(dimensions.y):
 			for col in range(dimensions.x):
 				var atlas_texture = AtlasTexture.new()
 				atlas_texture.atlas = texture
 				atlas_texture.region = Rect2i(col * Block.SIZE, row * Block.SIZE, Block.SIZE, Block.SIZE)
-				var button = TextureButton.new()
+	
+				var button = TileButton.new()
+				button.source_id = source_id
+				button.atlas_coords = Vector2i(col, row)
+				button.toggle_mode = true
 				button.size = Vector2i(Block.SIZE, Block.SIZE)
 				button.texture_normal = atlas_texture
+				button.button_group = button_group
+				
+				var outline = TextureRect.new()
+				outline.texture = outline_texture
+				outline.visible = false
+				outline.position = Vector2.ZERO
+				outline.mouse_filter = Control.MOUSE_FILTER_IGNORE
+				button.add_child(outline)
+
+				button.toggled.connect(func(pressed): outline.visible = pressed)
+
 				tile_container.add_child(button)
+
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		var local_pos = subviewport_container.get_local_mouse_position()
+		if local_pos.x < 0 or local_pos.y < 0 or local_pos.x > subviewport_container.size.x or local_pos.y > subviewport_container.size.y:
+			return
+		print(local_pos)
---
+++ editor/tile_button.gd
@@ -0,0 +1,5 @@
+extends TextureButton
+
+
+var source_id: int = -1
+var atlas_coords: Vector2i = Vector2i(-1, -1)
